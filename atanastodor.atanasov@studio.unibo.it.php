37767a19a3a7d4a825c01d56e5dce2ed782b87f1
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2021-12-17 19:16:30 +0100

    updates

--- atanastodor.atanasov@studio.unibo.it.php
+++ atanastodor.atanasov@studio.unibo.it.php
@@ -1,23 +1,54 @@
 <?php
+    class DatabaseHelper{
+        private $db;
+    
+        public function __construct($servername, $username, $password, $dbname, $port){
+            $this->db = new mysqli($servername, $username, $password, $dbname, $port);
+            if($this->db->connect_error){
+                die("Connesione fallita al db");
+            }
+        }
+    
+        public function getVal($v){
+            $stmt = $this->db->prepare("SELECT * FROM insiemi WHERE insieme=?");
+            $stmt->bind_param("i", $v);
+            $stmt->execute();
+            $result = $stmt->get_result();
+    
+            return $result->fetch_all(MYSQLI_ASSOC);
+        }
+    
+        public function getIns(){
+            $stmt = $this->db->prepare("SELECT * FROM insiemi");
+            $stmt->execute();
+            $result = $stmt->get_result();
+    
+            return $result->fetch_all(MYSQLI_ASSOC);
+        }
+    
+        public function getID(){
+            $stmt = $this->db->prepare("SELECT MAX(insieme) AS insieme FROM insiemi");
+            $stmt->execute();
+            $result = $stmt->get_result();
+    
+            return $result->fetch_object();
+        }
+
+        public function insertIns($id, $valore, $insieme){
+            $stmt = $this->db->prepare("INSERT INTO insiemi (id, valore, insieme) VALUES (?,?,?)");
+            $stmt->bind_param("iii", $id, $valore, $insieme);
+            $stmt->execute();
+        }
+    
+    }
+
     $host = "127.0.0.1";
     $user = "root";
     $password = "";
     $database = "giugno";
     $port = "3307";
 
-    $conn = new mysqli($host, $user, $password, $database, $port);
-
-    if($conn === false){
-        die("Errore di connesone: " . $conn->connect_error);
-    }
-
-    //echo("Connessione avvenuta con successo" . $conn->host_info);
-
-    /*function queryDB($query, $conn){
-        $result = mysqli_query($conn, $query);
-        return $result;
-    }*/
-
+    $conn = new DatabaseHelper($host, $user, $password, $database, $port);
 
     $a = $_GET['A'];
     $b = $_GET['B'];
@@ -29,10 +60,8 @@
     if((is_null($a) || is_null($b)) || ($a < 0 || $b < 0)){
         die("<br>Errore sui valori inseriti");
     } else {
-        $query = "select insieme from insiemi";
-        $result = mysqli_query($conn, $query);
-        
-        while($row = $result->fetch_assoc()){
+        $rows = $conn->getIns();
+        foreach($rows as $row){
             if($row["insieme"] == $a){
                 $isPresentA = true;
             }
@@ -40,7 +69,14 @@
                 $isPresentB = true;
             }
         }
-    
+        /*while($row = $result->fetch_assoc()){
+            if($row["insieme"] == $a){
+                $isPresentA = true;
+            }
+            if($row["insieme"] == $b){
+                $isPresentB = true;
+            }
+        }*/
         if(!$isPresentA || !$isPresentB){
             die("<br>I valori inseriti non sono presenti nell'insieme");
         }
@@ -56,15 +92,13 @@
     $vectorA = array();
     $vectorB = array();
 
-    $query = "select valore from insiemi where insieme = $a";
-    $result = mysqli_query($conn, $query);
-    while($row = $result->fetch_assoc()){
+    $rows = $conn->getVal($a);
+    foreach($rows as $row){
         $vectorA[] = $row["valore"];
     }
 
-    $query = "select valore from insiemi where insieme = $b";
-    $result = mysqli_query($conn, $query);
-    while($row = $result->fetch_assoc()){
+    $rows = $conn->getVal($b);
+    foreach($rows as $row){
         $vectorB[] = $row["valore"];
     }
     /*echo("<br>");
@@ -72,12 +106,16 @@
     echo("<br>");
     print_r($vectorB);*/
     if($o == 'u'){
-        $vector = array_merge($vectorA, $vectorB);
+        $vector = array_unique(array_merge($vectorA, $vectorB));
     } else {
         $vector = array_intersect($vectorA, $vectorB);
     }
     
     print_r($vector);
+
+    foreach($vector as $val){
+        $conn->insertIns($conn->getID, $val, 3);
+    }
 
     $conn->close();
 
